46cc09d6e0b7f80f710a2e0d19c7ba53a220c2c6
Dmitriy-Mashtaler <dmitriy.mashtaler.ksm@gmail.com>
2021-12-14 13:33:01 +0200

    before user part

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db={
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2021-12-14T11:28:09.610Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -5,32 +5,37 @@ admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
 
-exports.getScreams = functions.https.onRequest((request, response) => {
+const express = require("express");
+const app = express();
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+app.get('/scream',(request, response)=>{
     admin.firestore().collection('screams').get()
     .then(data =>{
         let screams = [];
         data.forEach(doc =>{
-            screams.push(doc.data());
+            screams.push({
+                screamId: doc.id,
+                body: doc.data().body,
+                userHandle: doc.data().userHandle,
+                createdAt: doc.data().createdAt
+            });
         });
         return response.json(screams);
     })
     .catch(err => console.error(err));
 });
 
-exports.createScream = functions.https.onRequest((request, response) => {
-    if(request.method !== 'POST'){
-        return response.status(400).json({ error: "Method not allowed" });
-    }
-    
+app.post('/scream/', ((request, response) => {
     const newScream = {
         body: request.body.body,
         userHandle: request.body.userHandle,
-        createAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()//admin.firestore.Timestamp.fromDate(new Date())
     };
 
     admin.firestore()
@@ -43,4 +48,41 @@ exports.createScream = functions.https.onRequest((request, response) => {
             response.status(500).json({error:`something went wrong`});
             console.error(err);
         })
-});
+}));
+
+// exports.getScreams = functions.https.onRequest((request, response) => {
+//     admin.firestore().collection('screams').get()
+//     .then(data =>{
+//         let screams = [];
+//         data.forEach(doc =>{
+//             screams.push(doc.data());
+//         });
+//         return response.json(screams);
+//     })
+//     .catch(err => console.error(err));
+// });
+
+// exports.createScream = functions.https.onRequest((request, response) => {
+//     if(request.method !== 'POST'){
+//         return response.status(400).json({ error: "Method not allowed" });
+//     }
+    
+//     const newScream = {
+//         body: request.body.body,
+//         userHandle: request.body.userHandle,
+//         createAt: admin.firestore.Timestamp.fromDate(new Date())
+//     };
+
+//     admin.firestore()
+//         .collection('screams')
+//         .add(newScream)
+//         .then(doc => {
+//             response.json({message: `document ${doc.id} created successfully`});
+//         })
+//         .catch(err=>{
+//             response.status(500).json({error:`something went wrong`});
+//             console.error(err);
+//         })
+// });
+
+exports.api = functions.https.onRequest(app);
